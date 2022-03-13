8f9e44beb53b7442e0fc06569602ccb7da1f12b1
Claire Wu <jjudy708618@gmail.com>
2022-03-14 00:54:17 +0800

    remove env

--- crawler/cli.py
+++ crawler/cli.py
@@ -12,17 +12,20 @@ def cli():
 @click.option("--country_id")
 @click.option("--size", default="default")
 @click.option("--schema_path")
-def klook_activity_pipeline(country_id, size, schema_path):
+@click.option("--schema_path")
+@click.option("--mongo_host")
+@click.option("--mongo_port")
+@click.option("--mongo_db")
+@click.option("--mongo_username")
+@click.option("--mongo_password")
+def klook_activity_pipeline(
+        country_id, size, schema_path, mongo_host, mongo_port, mongo_db, mongo_username=None,
+        mongo_password=None):
 
-    host = os.getenv('MongoHost')
-    port = os.getenv('Mongoport')
-    db = os.getenv('MongoDb')
-    username = os.getenv('MongoUserName', None)
-    password = os.getenv('MongoPassword', None)
-    click.echo(f"{host}, {port}, {username}, {password}, {db}")
+    click.echo(f"{mongo_host}, {mongo_port}, {mongo_username}, {mongo_password}, {mongo_db}")
     scp = KlookActivityPipeline(
-        country_id=country_id, size=int(size), schema_path=schema_path, host=host, port=int(port),
-        username=username, password=password, db=db
+        country_id=country_id, size=int(size), schema_path=schema_path, host=mongo_host, port=int(mongo_port),
+        username=mongo_username, password=mongo_password, db=mongo_db
     )
     scp.execute()
     click.echo("crawl activity loaded to mongo successfully!")
@@ -32,16 +35,19 @@ def klook_activity_pipeline(country_id, size, schema_path):
 @click.option("--activity_ids")
 @click.option("--size", default="50")
 @click.option("--schema_path")
-def klook_review_pipeline(activity_ids, size, schema_path):
-    host = os.getenv('MongoHost')
-    port = os.getenv('Mongoport')
-    db = os.getenv('MongoDb')
-    username = os.getenv('MongoUserName', None)
-    password = os.getenv('MongoPassword', None)
+@click.option("--mongo_host")
+@click.option("--mongo_port")
+@click.option("--mongo_db")
+@click.option("--mongo_username")
+@click.option("--mongo_password")
+def klook_review_pipeline(
+        activity_ids, size, schema_path, mongo_host, mongo_port, mongo_db, mongo_username=None,
+        mongo_password=None):
+
     activity_ids = eval(activity_ids)
     scp = KlookReviewPipeline(
-        activity_ids=activity_ids, size=int(size), schema_path=schema_path, host=host, port=int(port),
-        username=username, password=password, db=db
+        activity_ids=activity_ids, size=int(size), host=mongo_host, port=int(mongo_port),
+        username=mongo_username, password=mongo_password, db=mongo_db
     )
     scp.execute()
     click.echo("crawl review loaded to mongo successfully!")
